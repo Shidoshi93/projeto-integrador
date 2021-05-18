b7c9a06ad05a8808f081fa77840bce45971709cf
Patricia de Castro Araujo <patricia.decastroaraujo@gmail.com>
2021-05-17 21:54:55 -0300

    Tela Login

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,1 +1,38 @@
 // Página de login
+import {useState} from 'react'
+
+function Login() {
+    const [valuemail, setvaluemail] = useState()
+    const [valuesenha, setvaluesenha] = useState()
+
+    const onchangeemail = (event) => {
+
+            setvaluemail(event.target.value)
+            console.log(valuemail)
+    } 
+    
+    const onchangesenha = (event) => {
+
+        setvaluesenha(event.target.value)
+        console.log(valuesenha)
+} 
+    return(
+        <div>
+            Página de Login 
+            <form >
+                <label>
+                    Email:
+                </label>
+                <input onChange={onchangeemail} value={valuemail}></input>
+                <label>
+                    Senha:
+                </label>
+                <input type='password' onChange={onchangesenha} value={valuesenha}></input>
+                <button type='submit'>
+                    Acessar
+                </button>
+            </form>
+        </div>
+    )
+
+} export default Login
