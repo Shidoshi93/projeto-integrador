6fd566f00bc632c22e30f275567b1f6cf1e2bc8a
Patricia de Castro Araujo <patricia.decastroaraujo@gmail.com>
2021-05-17 22:33:01 -0300

    Criação Cadastro

--- src/pages/singup.js
+++ src/pages/singup.js
@@ -1,1 +1,50 @@
-// Página de cadastro
+// Página de cadastro// Página de login
+import {useState} from 'react'
+
+function Login() {
+    const [valuemail, setvaluemail] = useState()
+    const [valuesenha, setvaluesenha] = useState()
+    const [valuesenhaconfirma, setvaluesenhaconfirma] = useState()
+
+    const onchangeemail = (event) => {
+
+            setvaluemail(event.target.value)
+            console.log(valuemail)
+    } 
+    
+    const onchangesenha = (event) => {
+
+        setvaluesenha(event.target.value)
+        console.log(valuesenha)
+} 
+const onchangesenhaconfirma = (event) => {
+
+    setvaluesenhaconfirma(event.target.value)
+    console.log(valuesenhaconfirma)
+
+    return(
+        <div>
+            Cadastro do Usuário 
+            <form >
+                <label>
+                    Email:
+                </label>
+                <input onChange={onchangeemail} value={valuemail}></input>
+                <label>
+                    Senha:
+                </label>
+                <input type='password' onChange={onchangesenha} value={valuesenha}></input>
+                
+                <label>
+                    Confirme a Senha:
+                </label>
+                <input type='password' onChange={onchangesenhaconfirma} value={valuesenhaconfirma}></input>
+
+                <button type='submit'>
+                    Cadastrar
+                </button>
+            </form>
+        </div>
+    )
+
+} export default Login
