31af903c3307c1823613a61cf16589ba5241f212
Mariana Amina <mariana.dantasweb7@gmail.com>
2021-05-20 21:23:46 -0300

    Atualização da página de cadastro de doações adicionando campo de quantidade, campo de observação e campo para selecionar o tipo do usuário

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
+import React from 'react'
 import './App.css';
-import Titulo from './components/Titulo'
 import CadastroDoacao from './pages/singup2'
 
+// import Login from './pages/login';
+// function App() {
+//   return (
+//     <div className="App">
+//       Projeto integrador - Grupo 03
+//       <Login> 
 
-
-/*import Login from './pages/login';*/
-/*function App() {
-  return (
-    <div className="App">
-      Projeto integrador - Grupo 03
-      <Login> 
-
-      </Login>
-    </div>
-  );
-} export default App;*/
+//       </Login>
+//     </div>
+//   );
+// } export default App;
 
 function App() {
   return (
     <div className="App">
-      <Titulo>
-        
-      </Titulo>
       <CadastroDoacao> 
 
       </CadastroDoacao>
