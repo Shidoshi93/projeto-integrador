1ff9c37262c8d14a13b4553e9cd472cecc7af01d
shidoshi93 <dann.ribeiroo@gmail.com>
2021-05-30 01:14:42 +0100

    Alterado rota do botão de fazer login, para redirecionar para o feed.

--- src/pages/postDetail/style.js
+++ src/pages/postDetail/style.js
@@ -2,8 +2,7 @@ import styled from 'styled-components'
 
 export const MotherBox = styled.div`
     width: 80%;
-    margin: 0 auto;
-    margin-top: 50px;
+    margin: 50px auto;
     display: flex;
     flex-direction: column;
     gap: 30px;
