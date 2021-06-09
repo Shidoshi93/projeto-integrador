7e8e8648e7f8fc20d2a7861da21d8d5abeb109ba
Rachel Carvalho <rachelacarvalho@gmail.com>
2021-06-09 19:03:50 -0300

    Aplicação da API do ViaCep e alteração do css da pág de cadastro do usuário

--- src/components/singup/signupStyle.js
+++ src/components/singup/signupStyle.js
@@ -1,116 +1,103 @@
 import styled from 'styled-components';
 import '../../../src/fonts.css';
-const px2vw = (size, width = 1440) => `${(size / width) * 100}vw`;
 
 export const MotherBox = styled.div`
-    height: 80vh;
-    diplay: flex;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    // margin: 2%;
 `
 export const ContainerTitle = styled.div`
-    width: 20%;
-    height: 10%;
-    position: relative;
+    width: 25%;
     margin: 2% auto 1% auto;
     background: #F4F3F3;
 `
 export const Title = styled.h1`
-    width: 100%;
-    position: relative;
     font-family: Lato;
     font-style: normal;
     font-weight: 700;
-    font-size: ${px2vw(30)};
-    line-height: ${px2vw(40)};
+    font-size: 2.2rem;
+    line-height: 3rem;
     text-align: center;
-    padding-top: 2%;
+    padding: 2%;
     color: #F38D68;
 `
-export const ContainerForm = styled.form`
-    width: ${px2vw(580)};
-    height: ${px2vw(330)};    
-    position: relative;
-    display:flex; 
+export const ContainerForm = styled.div`
+    width: 45vw;
+    border: 1px solid #F38D68;
+    border-top-width: 15px;
+    background-color: #F4F3F3;
+    box-shadow: 10px 10px 0 rgba(0, 0, 0, 0.2);
+    border-radius: 5px;
+    margin: auto;
+    
+`    
+export const Form = styled.form`
+    width: 100%;
+    height: max-content;
+    display: flex; 
     flex-direction: column; 
     flex-wrap: wrap;
-    border: ${px2vw(1)} solid #F38D68;
-    border-top-width: ${px2vw(15)};
-    background-color: #F4F3F3;
-    box-shadow: ${px2vw(15)} ${px2vw(10)} ${px2vw(0)} rgba(0, 0, 0, 0.2);
-    border-radius: ${px2vw(5)};
-    margin: 0 auto;
-`    
+    margin: auto;
+    
+`
 export const FormDataContainer = styled.div`
-    width: ${px2vw(600)};
-    height: ${px2vw(350)};
-    position: relative;
+    width: 100%;
     display:flex; 
     flex-direction: row; 
     flex-wrap: wrap;
-    padding-left: 2.5%;
-    border: none;
+    padding: 2.5%;
+    margin: 0;
+    box-sizing: border-box;
 `
 export const Label = styled.label`
-    width: ${px2vw(150)};
-    height: ${px2vw(16)};
+    width: 30%;
     font-family: Lato;
     font-style: normal;
     font-weight: 300;
-    font-size: ${px2vw(16)};
-    line-height: ${px2vw(16)};
+    font-size: 1rem;
+    line-height: 1rem;
     text-align: left;
-    float:left;
-    margin-top:${px2vw(5)};
+    margin-top:1%;
 `
 export const Input = styled.input`
-    width: ${px2vw(400)};
-    height: ${px2vw(16)};
+    width: 70%;
+    height: 2em;
     background-color: #FFFFFF;
-    border: ${px2vw(1)} solid #F38D68;
-    border-radius: ${px2vw(10)};
+    border: 1px solid #F38D68;
+    border-radius: 10px;
     padding: 2%;
     box-sizing: border-box;
     font-family: Lato;
     font-style: normal;
     font-weight: normal;
-    font-size: ${px2vw(16)};
-    line-height: ${px2vw(16)};
-    margin-top:${px2vw(5)};
+    font-size: 1rem;
+    line-height: 1rem;
+    margin-top: .5%;
   `
-export const InputA = styled.input`
-    width: ${px2vw(300)};
-    height: ${px2vw(16)};
-    background-color: #FFFFFF;
-    border: ${px2vw(1)} solid #F38D68;
-    border-radius: ${px2vw(10)};
-    padding: 2%;
-    box-sizing: border-box;
-    font-family: Lato;
-    font-style: normal;
-    font-weight: normal;
-    font-size: ${px2vw(16)};
-    line-height: ${px2vw(16)};
-    margin-top:${px2vw(5)};
+export const InputA = styled(Input)`
+    width: 50%;
 `
 export const BtnCep = styled.button`
-    width: ${px2vw(80)};
-    height: ${px2vw(27)};
-    margin-left: ${px2vw(20)};
+    width: 15%;
+    height: 2em;
+    margin-left: 5%;
     border: none;
-    border-radius: ${px2vw(15)};
+    border-radius: 2em;
     background-color: #f38d68;
-    margin-top:${px2vw(5)};
+    margin-top: .5%;
     font-family: Lato;
     font-style: normal;
     font-weight: normal;
-    font-size: ${px2vw(16)};
-    line-height: ${px2vw(10)};
+    font-size: 1rem;
+    line-height: 1rem;
     text-align: center;
     color: #FFFFFF;
     cursor: pointer;
 
     &:active{
         outline: none;
-        border: ${px2vw(1)} solid grey;
+        border: 1px solid grey;
     }
 
     &:hover{
@@ -118,23 +105,22 @@ export const BtnCep = styled.button`
     }
 `
 export const FormBtnContainer = styled.div`
-    width: ${px2vw(650)};    
-    position: relative;
-    top: 10%;
-    display: inline-flex;
+    display:flex; 
+    flex-direction: row; 
+    flex-wrap: wrap;
     justify-content: space-evenly;
 `
 export const BtnSend = styled.button`
-    width: ${px2vw(140)};
-    height: ${px2vw(40)};
+    width: 30%;
+    height: 3em;
     border: none;
-    border-radius: ${px2vw(30)};
+    border-radius: 2em;
     background-color: #2BB67E;
     font-family: Lato;
     font-style: normal;
     font-weight: bold;
-    font-size: ${px2vw(16)};
-    line-height: ${px2vw(16)};
+    font-size: 1.08rem;
+    line-height: 1.08rem;
     text-align: center;
 
     color: #FFFFFF;
@@ -143,37 +129,16 @@ export const BtnSend = styled.button`
 
     &:active{
         outline: none;
-        border: ${px2vw(1)} solid black;
+        border: 1px solid black;
     }
 
     &:hover{
         background-color: #008000db;
     }
 `
-export const BtnClear = styled.button`
-    width: ${px2vw(140)};
-    height: ${px2vw(40)};
-    border: none;
-    border-radius: ${px2vw(30)};
+export const BtnClear = styled(BtnSend)`
     background-color: #F08080;
-    align-items: center;
-    display: inline-block;
-    align-content: center;
-    justify-content: center;
-    font-family: Lato;
-    font-style: normal;
-    font-weight: bold;
-    font-size: ${px2vw(16)};
-    line-height: ${px2vw(16)};
-    text-align: center;
-    color: #FFFFFF;
-    cursor: pointer;
-
-    &:active{
-        outline: none;
-        border: ${px2vw(1)} solid black;
-    }
-
+   
     &:hover{
         background-color: #e08361;
     }
