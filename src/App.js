2d325ae941709d5a47c99704630ab9166a04e29f
Shidoshi93 <dann.ribeiroo@gmail.com>
2021-07-19 05:41:02 +0100

    alterado altura do caontainer do feed.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './fonts.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Router from './routes/router'
-import Footer from './components/footer/footer'
+import Footer from './components/footer/Footer'
 import {Global} from './styles/global'
 import NavTest from './components/Nav/Nav';
 
