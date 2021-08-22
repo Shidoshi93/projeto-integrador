77dfc857d22d5d105990c5ceef649924c20731b1
Rachel Carvalho <rachelacarvalho@gmail.com>
2021-08-22 19:53:09 -0300

    Refatora o perfil e atualização de cadastro

--- src/components/userAccount/modal.js
+++ src/components/userAccount/modal.js
@@ -11,8 +11,11 @@ import {
     BtnCep,
     CloseModalButton
 } from './styleModal'
+import { BASE_URL } from '../../constants/constants';
+import jwt_decode from 'jwt-decode';
 
 export const Modal = ({ showModal, setShowModal }) => {
+    //MODAL HANDLER
     const modalRef = useRef();
 
     const closeModal = e => {
@@ -39,59 +42,280 @@ export const Modal = ({ showModal, setShowModal }) => {
         [keyPress]
     );
 
+    //VIACEP HANDLER
+    const [valuecep, setvaluecep] = useState('')
+    const [cepData, setCepData] = useState({})
     const [cep, setCep] = useState('');
-    const [cepData, setCepData] = useState({});
-    const [showCep, setShowCep] = useState(false);
+
 
     function handleChangeCep(event) {
-        setCep (event.target.value)
+        setCep(event.target.value)
     }
 
     function handleCepSubmit(event) {
         event.preventDefault();
 
-        axios.get (`https://viacep.com.br/ws/${cep}/json/`)
-        .then(res => res.data)
-        .then(res => setCepData(res), setShowCep(true))
-        .catch(erro => alert('Não foi possível localizar esse CEP'));
-        
+        axios.get(`https://viacep.com.br/ws/${cep}/json/`)
+            .then((res) => {
+                setCepData(res.data)
+                setBairro(res.data.bairro)
+                setCity(res.data.localidade)
+                setState(res.data.uf)
+            })
+            .catch(erro => alert('Não foi possível localizar esse CEP'));
     }
 
-     
+    //GET USER DATA
+    const [user, setUser] = useState();
+    const [userId, setUserId] = useState();
+
+    useEffect(() => {
+        getUser()
+    }, [])
+
+    const token = localStorage.getItem("token");
+    let decoded = jwt_decode(token);
+
+    const email = decoded.sub;
+
+    const getUser = async () => {
+        await axios.get(`${BASE_URL}/user/?email=${email}`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+            .then((res) => {
+                setUser(res.data)
+                setUserId(res.data.id)
+                setNome(res.data.user_name)
+                setPhone(res.data.cellphone)
+                setvalueemail(res.data.email)
+                localStorage.setItem("userFoto", (res.data.user_img));
+
+                console.log(res.data)
+            })
+            .catch((err) => {
+                console.log(err.message)
+            });
+    }
+
+    //USER HANDLER
+    const [valueemail, setvalueemail] = useState('')
+    const [nome, setNome] = useState('')
+    const [phone, setPhone] = useState('')
+    const [valuesenha, setvaluesenha] = useState('')
+    const [valuesenhaconfirma, setvaluesenhaconfirma] = useState('')
+    const [address, setAddress] = useState();
+    const [addressId, setAddressId] = useState();
+    const [bairro, setBairro] = useState();
+    const [city, setCity] = useState();
+    const [state, setState] = useState();
+
+
+    const onchangesenha = (event) => {
+        setvaluesenha(event.target.value)
+        console.log(valuesenha)
+    }
+
+    const onchangesenhaconfirma = (event) => {
+        setvaluesenhaconfirma(event.target.value)
+        console.log(valuesenhaconfirma)
+    }
+
+    const onchangeemail = (event) => {
+        setvalueemail(event.target.value)
+    }
+    const handleNome = (event) => {
+        setNome(event.target.value)
+    }
+
+    const handlePhone = (event) => {
+        setPhone(event.target.value)
+    }
+
+    const onchangebairro = (event) => {
+        setBairro(event.target.value)
+    }
+
+    const onchangecity = (event) => {
+        setCity(event.target.value)
+    }
+
+    const onchangestate = (event) => {
+        setState(event.target.value)
+    }
+
+    //IMG HANDLER (captura, converte a imagem para base64 e salva no localStorage)
+    const onchangeFoto = (event) => {
+        const file = event.target.files
+
+        let reader = new FileReader()
+        reader.readAsDataURL(file[0])
+
+        reader.onload = (event) => {
+            let item = event.target.result
+            console.log('foto', event.target.result)
+            localStorage.setItem('userFoto', item)
+        }
+    }
+
+    //GET USER ADDRESS
+    useEffect(() => {
+        getAddress()
+    }, [user]);
+
+    const getAddress = async () => {
+        await axios.get(`${BASE_URL}/address/userId/${userId}`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+            .then((res) => {
+                setAddress(res.data)
+                setCep(res.data.postalCode)
+                setAddressId(res.data.id)
+                setBairro(res.data.neighborhood)
+                setCity(res.data.city)
+                setState(res.data.city)
+
+                console.log(res.data)
+            })
+            .catch((err) => {
+                console.log(err.message)
+            })
+    }
+
+    //UPDATE HANDLER
+
+    const bodyUser = {
+        user_name: nome,
+        email: valueemail,
+        cellphone: phone,
+        user_img: localStorage.getItem('userFoto'),
+        password: valuesenhaconfirma,
+    }
+
+    const bodyAddress = {
+        neighborhood: bairro,
+        city: city,
+        state: state,
+        postalCode: cep,
+        user: {
+            id: userId
+        },
+    }
+
+    const updateUser = () => {
+
+        axios.patch(`${BASE_URL}/user/${userId}`, bodyUser, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+            .then((res) => {
+                alert("Cadastro atualizado")
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+
+        axios.patch(`${BASE_URL}/address/${addressId}`, bodyAddress, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+            .then((res) => { })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+
     return (
         <>
             {showModal ? (
                 <MainContainer onClick={closeModal} ref={modalRef}>
                     <CloseModalButton onClick={() => setShowModal(prev => !prev)}>
-                            X
+                        X
                     </CloseModalButton>
                     <Form showModal={showModal}>
                         <FormDataContainer>
-                            <Label>Nome completo: </Label>
-                            <Input placeholder="Rachel Carvalho" />
-                            <Label>E-mail: </Label>
-                            <Input placeholder="rachelacarvalho@gmail.com" />
+                            <Label htmlFor='nome'>Nome completo: </Label>
+                            <Input
+                                type='Text'
+                                name='nome'
+                                id='nome'
+                                value={nome}
+                                placeholder={user.user_name}
+                                onChange={handleNome}></Input>
+
+                            <Label
+                                htmlFor='foto'
+                            >Foto: </Label>
+                            <Input
+                                type='file'
+                                accept="image/png, image/jpeg"
+                                placeholder='Insira sua foto'
+                                name='foto'
+                                id='foto'
+                                onChange={(event) => onchangeFoto(event)}
+                            ></Input>
+                            <Label
+                                htmlFor='telefone'
+                            >Telefone: </Label>
+                            <Input
+                                type='Text'
+                                name='telefone'
+                                id='telefone'
+                                value={phone}
+                                placeholder={user.cellphone}
+                                onChange={handlePhone}
+                            ></Input>
+                            <Label htmlFor='email'>E-mail: </Label>
+                            <Input
+                                type='email'
+                                onChange={onchangeemail}
+                                value={valueemail}
+                                placeholder={user.email}
+                                name='email' id='email'></Input>
                             <Label>CEP: </Label>
                             <div>
-                                <InputA onChange={e => handleChangeCep(e)} value={cep} placeholder="21555-500" type="text" />
+                                <InputA
+                                    type="text"
+                                    onChange={e => handleChangeCep(e)}
+                                    value={cep}
+                                    placeholder={address ? address.postalCode : "Carregando dados do usuário..."} />
                                 <BtnCep type="submit" onClick={e => handleCepSubmit(e)}>Pesquisar</BtnCep>
                             </div>
-                            {showCep &&
-                            (
-                                <>
-                                    <Label>Bairro: </Label>
-                                    <Input value={cepData.bairro} />
-                                    <Label>Cidade: </Label>
-                                    <Input value={cepData.localidade} />
-                                    <Label>Estado: </Label>
-                                    <Input value={cepData.uf} />
-                                </>
-                        )
-                    }
-                   </FormDataContainer>
-                   <BtnForm>ALTERAR</BtnForm>        
+                            <Label>Bairro: </Label>
+                            <Input value={cepData.bairro}
+                                placeholder={address ? address.neighborhood : "Carregando dados do usuário..."}
+                                onChange={onchangebairro} />
+                            <Label>Cidade: </Label>
+                            <Input value={cepData.localidade}
+                                onChange={onchangecity}
+                                placeholder={address ? address.city : "Carregando dados do usuário..."} />
+                            <Label>Estado: </Label>
+                            <Input value={cepData.uf}
+                                onChange={onchangestate}
+                                placeholder={address ? address.state : "Carregando dados do usuário..."} />
+                            <Label htmlFor='senha'>Senha: </Label>
+                            <Input
+                                required
+                                onChange={onchangesenha}
+                                type='password'
+                                value={valuesenha}
+                                name='senha' id='senha'></Input>
+
+                            <Label htmlFor='confSenha'>Confirme sua senha: </Label>
+                            <Input
+                                required
+                                onChange={onchangesenhaconfirma}
+                                type='password' value={valuesenhaconfirma}
+                                name='confSenha' id='confSenha'></Input>
+                        </FormDataContainer>
+                        <BtnForm onClick={() => { if (window.confirm('Confirma a alteração do cadastro?')) { (updateUser()) } }}>ALTERAR</BtnForm>
                     </Form>
-                      
+
                 </MainContainer>
             ) : null}
         </>
--- src/components/userAccount/userAccount.js
+++ src/components/userAccount/userAccount.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router'
 import { useHistory } from 'react-router';
 import '../../../src/fonts.css';
 import { goTo } from '../../routes/coordinator';
@@ -22,28 +23,50 @@ import {
     BtnForm
 } from './styleUserAccount'
 import userFotoDefault from '../../images/userphoto.png'
+import axios from 'axios';
+import { BASE_URL } from '../../constants/constants';
+import jwt_decode from 'jwt-decode';
+
 
 function UserAccount() {
+    const [user, setUser] = useState();
+    const [userId, setUserId] = useState();
+    const [address, setAddress] = useState();
+
+    useEffect(() => {
+        getUser()
+    }, [])
+
+    const token = localStorage.getItem("token");
+    let decoded = jwt_decode(token);
+
+    const email = decoded.sub;
+
+    const getUser = async () => {
+        await axios.get(`${BASE_URL}/user/?email=${email}`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+            .then((res) => {
+                setUser(res.data)
+                setUserId(res.data.id)
+                console.log(res.data)
+            })
+            .catch((err) => {
+                console.log(err.message)
+            });
+    }
+
+
 
     const history = useHistory()
-    const userData = [
-        {
-            "userName": "Rachel",
-            "userLastname": "Carvalho",
-            "userFullName": "Rachel Carvalho",
-            "email": "rachelacarvalho@gmail.com",
-            "key": "Rachel123",
-            "bairro": "Marechal Hermes",
-            "cep": "21555-500",
-            "localidade": "Rio de Janeiro",
-            "uf": "RJ",
-            "cpf": "07889998899",
-            "img": "http://placekitten.com/200/200"
-        }]
 
     const [showForm, setShowForm] = useState(true);
 
     const [showModal, setShowModal] = useState(false);
+
+    const [showChangePassword, setShowChangePassword] = useState(false);
 
     const fotoUser = localStorage.getItem('userFoto')
 
@@ -51,68 +74,82 @@ function UserAccount() {
         setShowModal(prev => !prev);
     };
 
-    const [showChangePassword, setShowChangePassword] = useState(false);
-
     const openChangePassword = () => {
         setShowChangePassword(prev => !prev);
     };
+    useEffect(() => {
+        if (showModal || showChangePassword ? setShowForm(false) : setShowForm(true));
+    }, [showModal, showChangePassword]);
 
 
     useEffect(() => {
-        if (showModal || showChangePassword ? setShowForm(false) : setShowForm(true));;
-    }, [showModal, showChangePassword]);
+        getAddress()
+    }, [user]);
+
+    const getAddress = async () => {
+        await axios.get(`${BASE_URL}/address/userId/${userId}`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+            .then((res) => {
+                setAddress(res.data)
+                console.log(res.data)
+            })
+            .catch((err) => {
+                console.log(err.message)
+            })
+    }
 
     return (
         <div>
-            {userData.map((user) => (
-                <MotherBox>
-                    <HeadContainer>
-                        <HeadUserContainer>
-                            <ImgContainer id='userFoto' src={fotoUser ? fotoUser : userFotoDefault} alt={user.userFullName} />
-                            <Title>{user.userFullName}</Title>
-                        </HeadUserContainer>
-                        <HeadBtnContainer>
-                            <BtnHead onClick={() => goTo(history, '/donation')}>FAZER DOAÇÃO</BtnHead>
-                            <BtnHead onClick={() => goTo(history, '/donation')}>PEDIR DOAÇÃO</BtnHead>
-                        </HeadBtnContainer>
-                    </HeadContainer>
-                    <Modal showModal={showModal} setShowModal={setShowModal} />
-                    <ChangePassword showChangePassword={showChangePassword} setShowChangePassword={setShowChangePassword} />
-                    {showForm &&
-                        <>
-                            <MainContainer >
-                                <CardContainer>
+            <MotherBox>
+                <HeadContainer>
+                    <HeadUserContainer>
+                        <ImgContainer id='userFoto' src={fotoUser ? fotoUser : userFotoDefault} alt={user ? user.user_name : "Carregando dados do usuário..."} />
+                        <Title>{user ? user.user_name : "Carregando dados do usuário..."}</Title>
+                    </HeadUserContainer>
+                    <HeadBtnContainer>
+                        <BtnHead onClick={() => goTo(history, '/donation')}>FAZER DOAÇÃO</BtnHead>
+                        <BtnHead onClick={() => goTo(history, '/donation')}>PEDIR DOAÇÃO</BtnHead>
+                    </HeadBtnContainer>
+                </HeadContainer>
+                <Modal showModal={showModal} setShowModal={setShowModal} />
+                <ChangePassword showChangePassword={showChangePassword} setShowChangePassword={setShowChangePassword} />
+                {showForm &&
+                    <>
+                        <MainContainer >
+                            <CardContainer>
+                                <Form>
+                                    <FormDataContainer>
+                                        <Label>Nome completo: </Label>
+                                        <Input value={user ? user.user_name : "Carregando dados do usuário..."} />
+                                        <Label>Telefone: </Label>
+                                        <Input value={user ? user.cellphone : "Carregando dados do usuário..."} />
+                                        <Label>CPF: </Label>
+                                        <Input value={user ? user.cpf : "Carregando dados do usuário..."} />
+                                        <Label>E-mail: </Label>
+                                        <Input value={user ? user.email : "Carregando dados do usuário..."} />
+                                        <Label>CEP: </Label>
+                                        <Input value={address ? address.postalCode : "Carregando dados do usuário..."} />
+                                        <Label>Cidade: </Label>
+                                        <Input value={address ? address.city : "Carregando dados do usuário..."} />
+                                        <Label>Estado: </Label>
+                                        <Input value={address ? address.state : "Carregando dados do usuário..."} />
+                                        <Label>Bairro: </Label>
+                                        <Input value={address ? address.neighborhood : "Carregando dados do usuário..."} />
+                                    </FormDataContainer>
+                                </Form>
+                            </CardContainer>
+                        </MainContainer>
+                        <FormBtnContainer>
+                            <BtnForm onClick={openModal}>ALTERAR CADASTRO</BtnForm>
+                        </FormBtnContainer>
+                    </>
+                }
+            </MotherBox>
 
-                                    <Form>
-                                        <FormDataContainer>
-                                            <Label>Nome completo: </Label>
-                                            <Input value={user.userFullName} />
-                                            <Label>CPF: </Label>
-                                            <Input value={user.cpf} />
-                                            <Label>E-mail: </Label>
-                                            <Input value={user.email} />
-                                            <Label>CEP: </Label>
-                                            <Input value={user.cep} />
-                                            <Label>Cidade: </Label>
-                                            <Input value={user.localidade} />
-                                            <Label>Estado: </Label>
-                                            <Input value={user.uf} />
-                                            <Label>Bairro: </Label>
-                                            <Input value={user.bairro} />
-                                        </FormDataContainer>
-                                    </Form>
-                                </CardContainer>
-                            </MainContainer>
-                            <FormBtnContainer>
-                                <BtnForm onClick={openModal}>ALTERAR CADASTRO</BtnForm>
-                                <BtnForm onClick={openChangePassword}>ALTERAR SENHA</BtnForm>
-
-                            </FormBtnContainer>
-                        </>
-                    }
-                </MotherBox>
             )
-            )}
 
         </div>
     )
